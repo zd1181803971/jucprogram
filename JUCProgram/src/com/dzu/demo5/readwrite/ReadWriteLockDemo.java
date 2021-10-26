ca5cd06aafbd5c2863f16035869b42395f51c015
ZhaoDong <7942160+zhao_dong1181803971@user.noreply.gitee.com>
2021-10-26 22:13:26 +0800

    谷粒商城学习分布式锁

--- JUCProgram/src/com/dzu/demo5/readwrite/ReadWriteLockDemo.java
+++ JUCProgram/src/com/dzu/demo5/readwrite/ReadWriteLockDemo.java
@@ -10,6 +10,15 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  * @author by ZhaoDong
  * @Classname ReadWriteLock
  * @Description 读写锁 案例
+ * 一定能读到最新的数据
+ * 修改期间，写锁是一个排他锁（互斥锁、独享锁）
+ * 读锁是一个共享锁
+ * 写锁没释放，读就必须等待
+ * 读 + 读； 无锁的状态，并发读只会在redis中记录好当前的所有读锁，都会加锁成功。共享锁 大家都能用
+ * 写 + 读； 等待写锁释放
+ * 写 + 写； 阻塞的方式
+ * 读 + 写； 有读锁，写也需要等待
+ * 只要有写的存在，无论前后，都必须等待 【排他锁
  * @Date 2021/10/26 21:26
  */
 public class ReadWriteLockDemo {
