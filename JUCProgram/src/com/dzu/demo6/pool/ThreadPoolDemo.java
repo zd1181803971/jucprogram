40120ec7dc7da5763a6b84ed1ffb782845996a11
ZhaoDong <7942160+zhao_dong1181803971@user.noreply.gitee.com>
2021-10-29 23:40:31 +0800

    线程池基本用法

--- JUCProgram/src/com/dzu/demo6/pool/ThreadPoolDemo.java
+++ JUCProgram/src/com/dzu/demo6/pool/ThreadPoolDemo.java
@@ -10,16 +10,24 @@ import java.util.concurrent.*;
  */
 public class ThreadPoolDemo {
     public static void main(String[] args) {
-        ExecutorService pool = Executors.newFixedThreadPool(5);
+
+        // 一个池 5个线程
+//        ExecutorService pool = Executors.newFixedThreadPool(5);
+
+        // 阿里巴巴规范建立创建线程的方法
         ExecutorService pool2 = new ThreadPoolExecutor(1, 2, 1000, TimeUnit.MILLISECONDS,
                 new SynchronousQueue<Runnable>(), Executors.defaultThreadFactory(),
                 new ThreadPoolExecutor.AbortPolicy());
+        // 一池一线程
+//        ExecutorService pool = Executors.newSingleThreadExecutor();
 
+        // 弹性扩容
+        ExecutorService pool = Executors.newCachedThreadPool();
         for (int i = 0; i < 11; i++) {
             pool.execute(() -> {
-                System.out.println("123");
+                System.out.println(Thread.currentThread().getName() + ":123");
             });
-
         }
+        pool.shutdown();
     }
 }
